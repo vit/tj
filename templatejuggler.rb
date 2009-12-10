f540f176221f79a4adc9ddcf5e6d722670d73b34
vit <vit@a64.shiegin.ru>
2009-12-11 02:39:15 +0300

    added i18n possibility

--- templatejuggler.rb
+++ templatejuggler.rb
@@ -1,4 +1,5 @@
-%w[haml].each {|r| require r}
+# coding: UTF-8
+%w[haml yaml].each {|r| require r}
 
 class TemplateJuggler
 
@@ -9,9 +10,21 @@ class TemplateJuggler
 		def initialize path='./views'
 			@path = path
 		end
-		def get id
+		def get_template id
 			path = File.join(@path, "#{id}.haml")
 			File::exists?(path) && File::readable?(path) ? File::open(path, 'r'){ |file| file.read } : nil
+		end
+		def get_locale lang, id
+			lang = [lang.to_s] unless lang.is_a? Array
+			locale_data = nil
+			-> s {
+				locale_data = lang.inject({}) do |acc,l|
+					path = File.join(@path, "#{id}.#{l}.yaml")
+					data = File::exists?(path) && File::readable?(path) ? YAML.load_file( path ) : nil
+					data.is_a?(Hash) ? data.merge(acc) : acc
+				end unless locale_data
+				locale_data[s.to_s] if locale_data
+			}
 		end
 	end
 
@@ -33,34 +46,44 @@ class TemplateJuggler
 		attr_accessor :current_template
 		# Это если изнутри шаблона потребуется отрисовать другой шаблон
 		attr_accessor :callback_machine
-		def initialize machine=nil
+		attr_accessor :lang
+		attr_accessor :locale
+		def initialize machine=nil, args = {}
+			@args = args
 			@callback_machine = machine
 		end
 		def call id, args={}
-			@callback_machine.render id, args if callback_machine.respond_to?(:render)
+			@callback_machine.render id, (@args.merge args) if callback_machine.respond_to?(:render)
 		end
+		def gettext s
+			(@locale && @locale[s]) ? @locale[s] : s.to_s
+		end
+		alias_method :_, :gettext
 	end
 
 	# Объект, имеющий метод get и возвращающий шаблон по заданному id.
 	# Для самой TJ не важно, где именно хранятся шаблоны. 
-	attr_accessor :loader
-	def initialize loader=nil
-		@loader = loader || SimpleLoader.new
+	attr_accessor :template_loader
+	attr_accessor :locale_loader
+	def initialize loader=nil, locale=nil
+		@template_loader = template_loader || SimpleLoader.new
+		@locale_loader = locale_loader || @template_loader
 	end
 	def render id, args={}
 		id = id.to_s # id -- строка, имеющая вид полного пути ( "/aaa/bbb/ccc/fff" )
 		id_a = id.split '/'
 		base_a = id_a.dup
 		base_a.pop
-		scope = args[:scope] || Scope.new(self)
+		scope = args[:scope] || Scope.new(self, args)
+		scope.lang = args[:lang] if args[:lang]
 		locals = args[:locals] || {}
 		body = ''
 		loop do
 			id = id_a.join('/')
 			next_id = nil
-			templ = @loader.get id
-		#	p id
+			templ = @template_loader.get_template id
 			if templ
+				scope.locale = @locale_loader.get_locale scope.lang, id
 				scope.current_template = id
 				scope.next_template = nil
 				body = Haml::Engine.new(templ).render(scope, locals) { body }
